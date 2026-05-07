f1a4b96bf7842f0f713c8e8eb8baa47655adf4b1
carina-lobnig <carina.lobnig@stud.fhgr.ch>
2026-05-07 16:56:32 +0200

    Grundgerüst und Titelseite

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-console.log("blub");
+// API Daten laden
+async function loadParkhaus() {
+    const url = 'https://data.bs.ch/api/explore/v2.1/catalog/datasets/100088/records?limit=20'; // mit korrekter API-URL ersetzen
+    try {
+        const response = await fetch(url);
+        return await response.json();
+    } catch (error) {
+        console.error(error);
+        return false;
+    }
+}
+const parkhaus = await loadParkhaus();
+console.log(parkhaus); // gibt die Daten der API oder false in der Konsole aus
